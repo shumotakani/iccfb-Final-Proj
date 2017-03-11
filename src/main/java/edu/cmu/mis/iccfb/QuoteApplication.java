97bf8b0c07d38c21ab87f7d9181a0198f5809fa8
Shu Wang <oneshoe0411@gmail.com>
2017-03-10 23:46:51 -0500

    final commit

---
+++ src/main/java/edu/cmu/mis/iccfb/QuoteApplication.java
@@ -0,0 +1,13 @@
+package edu.cmu.mis.iccfb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class QuoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuoteApplication.class, args);
+	}
+}
